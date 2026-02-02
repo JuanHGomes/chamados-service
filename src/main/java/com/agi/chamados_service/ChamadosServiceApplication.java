b6bfdb696be77bcd9500e71d94147bd29bc02660
Ana Carolina Cordeiro Aguilar <ana.aguilar@agi.com.br>
2026-02-02 10:01:17 -0300

    first

---
+++ src/main/java/com/agi/chamados_service/ChamadosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agi.chamados_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadosServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agi/chamados_service/ChamadosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agi.chamados_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
