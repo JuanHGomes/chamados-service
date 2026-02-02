f5ae4d2af9801e0facddb649e2734150fca80e86
Juan <juan.henrique@agi.com.br>
2026-02-02 11:50:49 -0300

    CrCR-SCRUM-9 Criar DTO do evento para envio mensagem

---
+++ src/main/java/com/agi/chamados_service/dto/TicketEvent.java
@@ -0,0 +1,17 @@
+package com.agi.chamados_service.dto;
+
+import com.agi.chamados_service.enumerate.StatusEnum;
+
+import java.time.LocalDateTime;
+
+public class TicketEvent {
+    private String id;
+    private String title;
+    private String description;
+    private StatusEnum status;
+    private StatusEnum priority;
+    private LocalDateTime cratedAt;
+    private User user;
+    private Comments comments;
+
+}
